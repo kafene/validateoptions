ebc8636e62090f5c50fea134956719961c276e9f
kafene <kafene@users.noreply.github.com>
2017-06-03 19:57:22 -0400

    fix browserify process

--- dist/validateoptions.js
+++ dist/validateoptions.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.validateoptions = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
@@ -53,7 +53,7 @@ const VALID_TYPES = [
  *         shared by both requirements and options are not in the returned
  *         object.
  */
-exports.validateOptions = function validateOptions(options, requirements) {
+function validateOptions(options, requirements) {
     options = options || {};
     const validatedOptions = {};
 
@@ -110,7 +110,8 @@ exports.validateOptions = function validateOptions(options, requirements) {
     }
 
     return validatedOptions;
-};
+}
+exports.validateOptions = validateOptions;
 
 // Similar to typeof, except arrays, null and regexps are identified
 // by "array" and "null" and "regexp", not "object'.
@@ -230,4 +231,5 @@ function union(...arrays) {
     }, []);
 }
 
-},{}]},{},[1]);
+},{}]},{},[1])(1)
+});
